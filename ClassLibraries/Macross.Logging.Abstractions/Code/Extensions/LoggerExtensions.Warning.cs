d4f05a4b153f4199c7c0507a28e1c63193e4fba4
Mikel Blanchard <mblanchard@macrosssoftware.com>
2020-01-25 10:34:04 -0800

    Added logger Write helper extensions for our friends using EventId.

---
+++ ClassLibraries/Macross.Logging.Abstractions/Code/Extensions/LoggerExtensions.Warning.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace Microsoft.Extensions.Logging
+{
+	public static partial class LoggerExtensions
+	{
+		/// <summary>
+		/// Formats and writes a <see cref="LogLevel.Warning"/> log message.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="message">Format string of the log message.</param>
+		/// <param name="args">An object array that contains zero or more objects to format.</param>
+		public static void WriteWarning(
+			this ILogger logger,
+			string message,
+			params object?[]? args)
+			=> Write(logger, LogLevel.Warning, 0, (object?)null, null, message, args);
+
+		/// <summary>
+		/// Formats and writes a <see cref="LogLevel.Warning"/> log message.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="eventId">The event id associated with the log.</param>
+		/// <param name="message">Format string of the log message.</param>
+		/// <param name="args">An object array that contains zero or more objects to format.</param>
+		public static void WriteWarning(
+			this ILogger logger,
+			EventId eventId,
+			string message,
+			params object?[]? args)
+			=> Write(logger, LogLevel.Warning, eventId, (object?)null, null, message, args);
+
+		/// <summary>
+		/// Formats and writes a <see cref="LogLevel.Warning"/> log message.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="data">Data to be attached to the log entry.</param>
+		/// <param name="message">Format string of the log message.</param>
+		/// <param name="args">An object array that contains zero or more objects to format.</param>
+		public static void WriteWarning(
+			this ILogger logger,
+			object? data,
+			string? message,
+			params object?[]? args)
+			=> Write(logger, LogLevel.Warning, 0, data, null, message, args);
+
+		/// <summary>
+		/// Formats and writes a <see cref="LogLevel.Warning"/> log message.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="eventId">The event id associated with the log.</param>
+		/// <param name="data">Data to be attached to the log entry.</param>
+		/// <param name="message">Format string of the log message.</param>
+		/// <param name="args">An object array that contains zero or more objects to format.</param>
+		public static void WriteWarning(
+			this ILogger logger,
+			EventId eventId,
+			object? data,
+			string? message,
+			params object?[]? args)
+			=> Write(logger, LogLevel.Warning, eventId, data, null, message, args);
+
+		/// <summary>
+		/// Formats and writes a <see cref="LogLevel.Warning"/> log message.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="exception">The exception to log.</param>
+		public static void WriteWarning(
+			this ILogger logger,
+			Exception? exception)
+			=> Write(logger, LogLevel.Warning, 0, (object?)null, exception, null, null);
+
+		/// <summary>
+		/// Formats and writes a <see cref="LogLevel.Warning"/> log message.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="eventId">The event id associated with the log.</param>
+		/// <param name="exception">The exception to log.</param>
+		public static void WriteWarning(
+			this ILogger logger,
+			EventId eventId,
+			Exception? exception)
+			=> Write(logger, LogLevel.Warning, eventId, (object?)null, exception, null, null);
+
+		/// <summary>
+		/// Formats and writes a <see cref="LogLevel.Warning"/> log message.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="exception">The exception to log.</param>
+		/// <param name="message">Format string of the log message.</param>
+		/// <param name="args">An object array that contains zero or more objects to format.</param>
+		public static void WriteWarning(
+			this ILogger logger,
+			Exception? exception,
+			string? message,
+			params object?[]? args)
+			=> Write(logger, LogLevel.Warning, 0, (object?)null, exception, message, args);
+
+		/// <summary>
+		/// Formats and writes a <see cref="LogLevel.Warning"/> log message.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="eventId">The event id associated with the log.</param>
+		/// <param name="exception">The exception to log.</param>
+		/// <param name="message">Format string of the log message.</param>
+		/// <param name="args">An object array that contains zero or more objects to format.</param>
+		public static void WriteWarning(
+			this ILogger logger,
+			EventId eventId,
+			Exception? exception,
+			string? message,
+			params object?[]? args)
+			=> Write(logger, LogLevel.Warning, eventId, (object?)null, exception, message, args);
+
+		/// <summary>
+		/// Formats and writes a <see cref="LogLevel.Warning"/> log message.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="data">Data to be attached to the log entry.</param>
+		/// <param name="exception">The exception to log.</param>
+		/// <param name="message">Format string of the log message.</param>
+		/// <param name="args">An object array that contains zero or more objects to format.</param>
+		public static void WriteWarning(
+			this ILogger logger,
+			object? data,
+			Exception? exception,
+			string? message,
+			params object?[]? args)
+			=> Write(logger, LogLevel.Warning, 0, data, exception, message, args);
+
+		/// <summary>
+		/// Formats and writes a <see cref="LogLevel.Warning"/> log message.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="eventId">The event id associated with the log.</param>
+		/// <param name="data">Data to be attached to the log entry.</param>
+		/// <param name="exception">The exception to log.</param>
+		/// <param name="message">Format string of the log message.</param>
+		/// <param name="args">An object array that contains zero or more objects to format.</param>
+		public static void WriteWarning(
+			this ILogger logger,
+			EventId eventId,
+			object? data,
+			Exception? exception,
+			string? message,
+			params object?[]? args)
+			=> Write(logger, LogLevel.Warning, eventId, data, exception, message, args);
+	}
+}
---
+++ ClassLibraries/Macross.Logging.Abstractions/Code/Extensions/LoggerExtensions.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+using MacrossLogging = Macross.Logging.Abstractions;
+
+namespace Microsoft.Extensions.Logging
+{
+	/// <summary>
+	/// ILogger extension methods for common scenarios.
+	/// </summary>
+	public static partial class LoggerExtensions
+	{
+		private static readonly Func<MacrossLogging.FormattedLogValues, Exception, string> s_MessageFormatter = MessageFormatter;
+
+		/// <summary>
+		/// Begins a logical operation group.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="groupName">Group name.</param>
+		/// <returns>An <see cref="IDisposable"/> that ends the logical operation group on dispose.</returns>
+		public static IDisposable BeginGroup(this ILogger logger, string groupName)
+		{
+			if (logger == null)
+				throw new ArgumentNullException(nameof(logger));
+
+			return logger.BeginScope(new LoggerGroup(groupName));
+		}
+
+		/// <summary>
+		/// Formats and writes a log message.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="logLevel">Entry will be written on this level.</param>
+		/// <param name="data">Data to be attached to the log entry.</param>
+		/// <param name="message">Format string of the log message.</param>
+		/// <param name="args">An object array that contains zero or more objects to format.</param>
+		public static void Write(
+			this ILogger logger,
+			LogLevel logLevel,
+			object? data,
+			string? message,
+			params object?[]? args)
+		{
+			if (logger == null)
+				throw new ArgumentNullException(nameof(logger));
+
+			logger.Log(logLevel, 0, new MacrossLogging.FormattedLogValues(message, data, args), null, s_MessageFormatter);
+		}
+
+		/// <summary>
+		/// Formats and writes a log message at the specified log level.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="logLevel">Entry will be written on this level.</param>
+		/// <param name="data">Data to be attached to the log entry.</param>
+		/// <param name="exception">The exception to log.</param>
+		/// <param name="message">Format string of the log message.</param>
+		/// <param name="args">An object array that contains zero or more objects to format.</param>
+		public static void Write(
+			this ILogger logger,
+			LogLevel logLevel,
+			object? data,
+			Exception? exception,
+			string? message,
+			params object?[]? args)
+		{
+			if (logger == null)
+				throw new ArgumentNullException(nameof(logger));
+
+			logger.Log(logLevel, 0, new MacrossLogging.FormattedLogValues(message, data, args), exception, s_MessageFormatter);
+		}
+
+		/// <summary>
+		/// Formats and writes a log message at the specified log level.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> to write to.</param>
+		/// <param name="logLevel">Entry will be written on this level.</param>
+		/// <param name="eventId">The event id associated with the log.</param>
+		/// <param name="data">Data to be attached to the log entry.</param>
+		/// <param name="exception">The exception to log.</param>
+		/// <param name="message">Format string of the log message.</param>
+		/// <param name="args">An object array that contains zero or more objects to format.</param>
+		public static void Write(
+			this ILogger logger,
+			LogLevel logLevel,
+			EventId eventId,
+			object? data,
+			Exception? exception,
+			string? message,
+			params object?[]? args)
+		{
+			if (logger == null)
+				throw new ArgumentNullException(nameof(logger));
+
+			logger.Log(logLevel, eventId, new MacrossLogging.FormattedLogValues(message, data, args), exception, s_MessageFormatter);
+		}
+
+		private static string MessageFormatter(MacrossLogging.FormattedLogValues state, Exception error) => state.ToString();
+	}
+}
