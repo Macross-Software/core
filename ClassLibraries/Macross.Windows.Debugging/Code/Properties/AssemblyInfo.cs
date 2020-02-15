0a5017ba3654bc6c4ce6ff327e1a03552a9ecb52
Mikel Blanchard <mblanchard@macrosssoftware.com>
2020-02-15 13:46:40 -0800

    Bumped version and julians.

--- ClassLibraries/Macross.Windows.Debugging/Code/Properties/AssemblyInfo.cs
+++ ClassLibraries/Macross.Windows.Debugging/Code/Properties/AssemblyInfo.cs
@@ -7,4 +7,4 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("7936396a-8e58-47ba-835a-1b2ec7369ede")]
 
-[assembly: AssemblyVersion("1.0.0.20026")]
+[assembly: AssemblyVersion("1.1.0.20026")]
