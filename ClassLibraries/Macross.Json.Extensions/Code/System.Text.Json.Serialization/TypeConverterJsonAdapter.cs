dc8240a2bb6331cc65db559659b41f126cf767b1
Konstantin Sharon <NN---@users.noreply.github.com>
2021-03-17 22:51:17 +0200

    Add TypeConverterJsonAdapter.

---
+++ ClassLibraries/Macross.Json.Extensions/Code/System.Text.Json.Serialization/TypeConverterJsonAdapter.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace System.Text.Json.Serialization
+{
+	/// <summary>
+	/// Adapter between <see cref="TypeConverter"/> and <see cref="JsonConverter"/>.
+	/// </summary>
+	/// <typeparam name="T">The type being converted.</typeparam>
+	public class TypeConverterJsonAdapter<T> : JsonConverter<T>
+	{
+		/// <inheritdoc/>
+		public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		{
+			TypeConverter converter = TypeDescriptor.GetConverter(typeToConvert);
+			string text = reader.GetString();
+			return (T)converter.ConvertFromString(text);
+		}
+
+		/// <inheritdoc/>
+		public override void Write(Utf8JsonWriter writer, T objectToWrite, JsonSerializerOptions options)
+		{
+			if (writer is null)
+				throw new ArgumentNullException(nameof(writer));
+
+			TypeConverter converter = TypeDescriptor.GetConverter(objectToWrite);
+			string text = converter.ConvertToString(objectToWrite);
+			writer.WriteStringValue(text);
+		}
+
+		/// <inheritdoc/>
+		public override bool CanConvert(Type typeToConvert)
+		{
+			bool hasConverter = typeToConvert.GetCustomAttributes<TypeConverterAttribute>(inherit: true).Any();
+			return hasConverter;
+		}
+	}
+}
---
+++ ClassLibraries/Macross.Json.Extensions/Code/System.Text.Json.Serialization/TypeConverterJsonAdapterFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace System.Text.Json.Serialization
+{
+	/// <summary>
+	/// A factory used to create various <see cref="TypeConverterJsonAdapter{T}"/> instances.
+	/// </summary>
+	public class TypeConverterJsonAdapterFactory : JsonConverterFactory
+	{
+		/// <inheritdoc />
+		public override bool CanConvert(Type typeToConvert)
+		{
+			bool hasConverter = typeToConvert.GetCustomAttributes<TypeConverterAttribute>(inherit: true).Any();
+			return hasConverter;
+		}
+
+		/// <inheritdoc />
+		public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+		{
+			Type converterType = typeof(TypeConverterJsonAdapter<>).MakeGenericType(typeToConvert);
+			return (JsonConverter)Activator.CreateInstance(converterType);
+		}
+	}
+}
