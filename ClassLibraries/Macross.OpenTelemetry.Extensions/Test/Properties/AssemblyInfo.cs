29930000674b80ce644712f354c48bad74c3f792
Mikel Blanchard <mblanchard@macrosssoftware.com>
2021-03-28 20:47:39 -0700

    Warning cleanup.

---
+++ ClassLibraries/Macross.OpenTelemetry.Extensions/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+
+[assembly: Guid("beb815de-41a4-4cfe-80eb-a5a1d64559c5")]
+
+[assembly: AssemblyVersion("0.0.0.21087")]
